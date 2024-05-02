05a171bd82e19b65f52cff6d9dccc361833a9681
threeD1415926 <118429320+threeD1415926@users.noreply.github.com>
2024-05-02 23:41:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -191,13 +191,14 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
             birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
         # 将生日数据插入data
         data["data"][key] = {"value": birthday_data, "color": get_color()}
+        print(birthday_data)
+        print(data["data"][key])
     headers = {
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
     response = post(url, headers=headers, json=data).json()
-    print(response)
     if response["errcode"] == 40037:
         print("推送消息失败，请检查模板id是否正确")
     elif response["errcode"] == 40036:
