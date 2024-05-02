b574a229622bf4eaf58cf12b551a01d375627643
threeD1415926 <118429320+threeD1415926@users.noreply.github.com>
2024-05-02 23:06:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def get_weather(region):
  
  
 def get_birthday(birthday, year, today):
-    print("计算生日")
+    print("birthday:" + birthday)
     birthday_year = birthday.split("-")[0]
     # 判断是否为农历生日
     if birthday_year[0] == "r":
@@ -81,6 +81,7 @@ def get_birthday(birthday, year, today):
         year_date = date(year, birthday_month, birthday_day)
  
     else:
+        print("计算国历生日")
         # 获取国历生日的今年对应月和日
         birthday_month = int(birthday.split("-")[1])
         birthday_day = int(birthday.split("-")[2])
@@ -100,6 +101,7 @@ def get_birthday(birthday, year, today):
     else:
         birth_date = year_date
         birth_day = str(birth_date.__sub__(today)).split(" ")[0]
+     print("计算完成：" + birth_day)
     return birth_day
  
  
@@ -177,6 +179,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         }
     }
     for key, value in birthdays.items():
+       print("key:" + key + "   value:" + value)
         # 获取距离下次生日的时间
         birth_day = get_birthday(value["birthday"], year, today)
         if birth_day == 0:
